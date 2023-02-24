bf00fe755b143463726ec6af206dde15dedb36ca
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-02-24 13:35:50 +0100

    Added AppHero component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script></script>
+
+<template>
+  <div>
+    <h2>Jumbotron content</h2>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url("../assets/wallpapersden.com_cat-lying-cool-cat_1920x1080.jpg");
+  height: 80vh;
+  background-size: cover;
+}
+
+h2 {
+  font-size: 3rem;
+}
+</style>
